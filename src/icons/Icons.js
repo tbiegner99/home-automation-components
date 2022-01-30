ec7a4626dff2caf9edee45e7eaa0596cf1a4e4f2
TJ Biegner <tbiegner99@gmail.com>
2022-01-30 01:17:33 -0500

    add form modules and icons

--- src/icons/Icons.js
+++ src/icons/Icons.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSync,faTrash, faCaretUp,faCaretDown,faHome} from "@fortawesome/free-solid-svg-icons"
+import { faSync,faTrash, faCaretUp,faCaretDown,faHome,faPlus } from "@fortawesome/free-solid-svg-icons"
 
 export const createIcon = (icon) => (props) => <i {...props} ><FontAwesomeIcon icon={icon}/></i>;
 export const createGraphicIcon = (icon) => (props) => <g {...props} ><FontAwesomeIcon icon={icon}/></g>;
@@ -11,3 +11,4 @@ export const DeleteIcon = createIcon(faTrash);
 export const MoveUpIcon = createIcon(faCaretUp);
 export const MoveDownIcon = createIcon(faCaretDown);
 export const HomeIcon = createIcon(faHome);
+export const AddIcon = createIcon(faPlus);
--- src/inputs/index.js
+++ src/inputs/index.js
@@ -2,4 +2,4 @@
 
 export { default as TextArea } from './TextArea.js';
 export { default as TextInput } from './TextInput.js';
-
+export {Form, HiddenField,Button} from '@tbiegner99/react-forms' 
